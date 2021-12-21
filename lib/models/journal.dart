3517f90bd5d31a12586b746e2185ba3144dc6d49
mauricio.santana <mauricio.santana@nubank.com.br>
2021-12-20 21:03:53 -0300

    remove not more used code.

--- lib/models/journal.dart
+++ lib/models/journal.dart
@@ -1,31 +1,3 @@
-class OldJournal {
-  OldJournal({
-    required this.id,
-    required this.date,
-    required this.mood,
-    required this.note,
-  });
-
-  final String id;
-  final String date;
-  final String mood;
-  final String note;
-
-  factory OldJournal.fromJson(Map<String, dynamic> json) => OldJournal(
-        id: json['id'],
-        date: json['date'],
-        mood: json['mood'],
-        note: json['note'],
-      );
-
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'date': date,
-        'mood': mood,
-        'note': note,
-      };
-}
-
 class Journal {
   Journal({
     required this.documentID,
